e53e23b5b3cae767dfb56bfb7e0737b4fd486cba
Yonad89 <144005909+Yonad89@users.noreply.github.com>
2026-09-09 00:55:36 +0300

    Update Hero3D.tsx

--- src/Hero3D.tsx
+++ src/Hero3D.tsx
@@ -3,19 +3,19 @@ import Spline from '@splinetool/react-spline';
 export default function Hero3D() {
   return (
     <div className="fixed inset-0 w-screen h-screen -z-10 bg-[#0c0d0e] dark:bg-slate-900 overflow-hidden">
-      {/* Secondary Ambient Scene (Background) */}
-      <div className="absolute inset-0 z-0 opacity-60 pointer-events-none">
+      {/* Background Secondary Scene */}
+      <div className="absolute inset-0 z-0 w-full h-full pointer-events-none opacity-70">
         <Spline 
           scene="https://prod.spline.design/jnGeSPeYb0orxDYP/scene.splinecode" 
-          style={{ width: '100vw', height: '100vh' }}
+          style={{ width: '100%', height: '100%', background: 'transparent' }}
         />
       </div>
 
-      {/* Primary Animation (Foreground) */}
+      {/* Foreground Primary Scene */}
       <div className="absolute inset-0 z-10 w-full h-full">
         <Spline 
           scene="https://prod.spline.design/eCPaVYkkdUPNfSTx/scene.splinecode" 
-          style={{ width: '100vw', height: '100vh' }}
+          style={{ width: '100%', height: '100%', background: 'transparent' }}
         />
       </div>
     </div>
