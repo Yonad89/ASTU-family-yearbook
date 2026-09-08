6500691110ceeb143a43c137b384d1595c690a5f
Yonad89 <144005909+Yonad89@users.noreply.github.com>
2026-09-08 23:18:03 +0300

    Update Hero3D.tsx

--- src/Hero3D.tsx
+++ src/Hero3D.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 export default function Hero3D() {
   return (
-    <div className="h-[400px] w-full relative overflow-hidden rounded-2xl bg-slate-900 my-6">
+    <div className="fixed inset-0 w-full h-full -z-10 pointer-events-none">
       <Spline scene="https://prod.spline.design/eCPaVYkkdUPNfSTx/scene.splinecode" />
     </div>
   );
