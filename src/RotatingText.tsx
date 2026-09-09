69ff7bf3d1f5608a636028bc4144bf5eaee3b458
Yonad89 <144005909+Yonad89@users.noreply.github.com>
2026-09-09 11:24:53 +0300

    Update RotatingText.tsx

--- src/RotatingText.tsx
+++ src/RotatingText.tsx
@@ -200,22 +200,23 @@ const RotatingText = forwardRef<RotatingTextRef, RotatingTextProps>(
               return (
                 <span key={wordIndex} className={cn('inline-flex', splitLevelClassName)}>
                   {wordObj.characters.map((char, charIndex) => (
-                    <motion.span
-                      key={charIndex}
-                      initial={initial}
-                      animate={animate}
-                      exit={exit}
-                      transition={{
-                        ...transition,
-                        delay: getStaggerDelay(
-                          previousCharsCount + charIndex,
-                          array.reduce((sum, word) => sum + word.characters.length, 0)
-                        )
-                      }}
-                      className={cn('inline-block', elementLevelClassName)}
-                    >
-                      {char}
-                    </motion.span>
+                 <motion.span
+  key={charIndex}
+  initial={initial}
+  animate={animate}
+  exit={exit}
+  style={{ willChange: 'transform, opacity' }}
+  transition={{
+    ...transition,
+    delay: getStaggerDelay(
+      previousCharsCount + charIndex,
+      array.reduce((sum, word) => sum + word.characters.length, 0)
+    )
+  }}
+  className={cn('inline-block transform-gpu', elementLevelClassName)}
+>
+  {char}
+</motion.span>
                   ))}
                   {wordObj.needsSpace && <span className="whitespace-pre"> </span>}
                 </span>
